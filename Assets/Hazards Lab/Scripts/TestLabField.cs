f17e755db8f94879d0bc07873f6920032b7a5b45
AUEzzat <amr.m.ezzat@gmail.com>
2018-11-18 11:44:36 +0200

    Init 2

---
+++ Assets/Hazards Lab/Scripts/TestLabField.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Hazards Lab/Test Lab Field")]
+public class TestLabField : AbstractField<TestLabEnum> {
+}
