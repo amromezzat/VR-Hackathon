0beefad8fb131478c159ffc602e739fc8f2005e5
Omar sleam <ooomar92@gmail.com>
2018-11-18 20:52:00 +0200

    some scripts and prefabs fixing

---
+++ Assets/Hazards Lab/Scripts/HazardShipSlotScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardShipSlotScript : MonoBehaviour {
+
+
+    public GameObjectField selectedHazard;
+    //public GameEvent RoomSelected;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        HazardShipScript hazardShipScript = null;
+        if (hazardShipScript = collision.gameObject.GetComponent<HazardShipScript>())
+        {
+            hazardShipScript.transform.position = this.transform.position;
+            hazardShipScript.transform.rotation = this.transform.rotation;
+
+            selectedHazard.Value = hazardShipScript.HazardRoomPrefab;
+            
+        }
+
+
+
+    }
+}
