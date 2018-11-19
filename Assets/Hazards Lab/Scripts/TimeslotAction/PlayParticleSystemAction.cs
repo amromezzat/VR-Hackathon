5f6a2901c911d6490b8464e5229be059b8a40d9d
AUEzzat <amr.m.ezzat@gmail.com>
2018-11-19 13:56:39 +0200

    change 3

---
+++ Assets/Hazards Lab/Scripts/TimeslotAction/PlayParticleSystemAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Hazards Lab/Actions/Play Particle System Action")]
+public class PlayParticleSystemAction : TimeslotAction
+{
+    ParticleSystem particleSystem;
+    public override void Initialize(GameObject gameObject)
+    {
+        particleSystem = gameObject.GetComponent<ParticleSystem>();
+    }
+
+    public override void TakeAction(float time)
+    {
+        particleSystem.Play(true);
+    }
+}
